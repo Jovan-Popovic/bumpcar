0daaaedfb3f5c0d7235d25564658e58ddfa426c7
@jovan.popovic <jovan.p@coinis.com>
2022-02-27 18:08:40 +0100

    Hotfix: Removed Django staticfiles to reduce project size

--- bumpcar/settings.py
+++ bumpcar/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "static/"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
