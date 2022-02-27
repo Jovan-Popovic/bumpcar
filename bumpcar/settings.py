af6bad3607b8f198516b4df314f4cb1d3a8339c9
@jovan.popovic <jovan.p@coinis.com>
2022-02-27 14:46:29 +0100

    Hotfix: Converted debug .env to boolean

--- bumpcar/settings.py
+++ bumpcar/settings.py
@@ -15,7 +15,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("DEBUG")
+DEBUG = bool(getenv("DEBUG"))
 
 ALLOWED_HOSTS = ["bumpcar-api.herokuapp.com", "127.0.0.1", "localhost"]
 
