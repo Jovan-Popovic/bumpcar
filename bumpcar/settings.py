bb06e0785db682e0537c7a4a1bab955c8236c8c5
Your Name <crownchiper@gmail.com>
2022-03-01 16:52:48 +0100

    cors7

--- bumpcar/settings.py
+++ bumpcar/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ["bumpcar-api.herokuapp.com", "127.0.0.1", "localhost", "0.0.0.0
 
 CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
     "GET",
