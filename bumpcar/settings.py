e7472089c4b82553273d0848c0b0b4dd765ceebd
Your Name <crownchiper@gmail.com>
2022-03-01 15:20:59 +0100

    production database

--- bumpcar/settings.py
+++ bumpcar/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG")
 
-ALLOWED_HOSTS = ["bumpcar-api.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["bumpcar-api.herokuapp.com", "127.0.0.1", "localhost", "0.0.0.0"]
 
 
 # Application definition
