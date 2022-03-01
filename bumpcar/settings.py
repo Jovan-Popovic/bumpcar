3deaa79352a0137c01c8316606a682a83f25a631
Your Name <crownchiper@gmail.com>
2022-03-01 14:35:42 +0100

    dat

--- bumpcar/settings.py
+++ bumpcar/settings.py
@@ -8,7 +8,8 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / ".env")
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
