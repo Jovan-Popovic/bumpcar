a02ee59f1e75a58f5461f1a44270580497930df5
Your Name <crownchiper@gmail.com>
2022-03-06 02:48:46 +0100

    pk

--- base/serializers.py
+++ base/serializers.py
@@ -66,7 +66,7 @@ class ProfileUpdateSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         profile_id = self.context['request'].parser_context.get('kwargs').get('pk')
 
-        Profile.objects.filter(user = profile_id).update(**validated_data)
+        Profile.objects.filter(pk = profile_id).update(**validated_data)
 
         # for key in validated_data:
             # setattr(instance, key, validated_data.get(key))
